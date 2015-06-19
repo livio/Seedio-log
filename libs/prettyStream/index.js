0b664834e130156464d39d5ed6668b2206d5596f
Corey Maylone <corey@livio.io>
2015-06-19 14:01:35 -0400

    Removed global variables

--- libs/prettyStream/index.js
+++ libs/prettyStream/index.js
@@ -2,33 +2,32 @@
  * ******************** Pretty prints a bunyan stream (now with 100% more color) using console.log
  * ************************************************** */
 
-var colors = require('colors');
-
-colors.setTheme({
-  success: 'green',
-  info: 'cyan',
-  trace: 'gray',
-  debug: 'magenta',
-  warn: 'yellow',
-  error: 'red'
-});
-
-// Logging levels. These correlate directly with how the bunyan logging library defines its logging levels.
-var levels = {
-  10: createLevel('TRACE', 10, 'trace'),
-  20: createLevel('DEBUG', 20, 'debug'),
-  30: createLevel('INFO', 30, 'info'),
-  40: createLevel('WARN', 40, 'warn'),
-  50: createLevel('ERROR', 50, 'error'),
-  60: createLevel('FATAL', 60, 'error')
-};
-
 var PrettyStream = function() {
+  this.colors = require('colors');
+  this.colors.setTheme({
+    success: 'green',
+    info: 'cyan',
+    trace: 'gray',
+    debug: 'magenta',
+    warn: 'yellow',
+    error: 'red'
+  });
+
+  // Logging levels. These correlate directly with how the bunyan logging library defines its logging levels.
+  this.levels = {
+    10: createLevel('TRACE', 10, 'trace'),
+    20: createLevel('DEBUG', 20, 'debug'),
+    30: createLevel('INFO', 30, 'info'),
+    40: createLevel('WARN', 40, 'warn'),
+    50: createLevel('ERROR', 50, 'error'),
+    60: createLevel('FATAL', 60, 'error')
+  };
+
   this.writable = true;
 };
 
 PrettyStream.prototype.write = function(obj) {
-  var level = levels[obj.level];
+  var level = this.levels[obj.level];
 
   // Handle pretty printing of HTTP request logs.
   if(obj.req) {
@@ -41,18 +40,18 @@ PrettyStream.prototype.write = function(obj) {
       color = 'error';
     }
 
-    console.log('[%s] ' + colors[color]('%s') + ' %s %s %s ms', obj.time.toUTCString(), obj.res.statusCode, obj.req.method, obj.req.url, obj['response-time'])
+    console.log('[%s] ' + this.colors[color]('%s') + ' %s %s %s ms', obj.time.toUTCString(), obj.res.statusCode, obj.req.method, obj.req.url, obj['response-time'])
   } else {
     if(obj.err) {
       // Log errors and their stacks.
-      console.log('[%s] ' + colors[level.color]('[%s]') + ': ' + colors[level.color]('%s') + '\n' + colors[level.color]('%s'),
+      console.log('[%s] ' + this.colors[level.color]('[%s]') + ': ' + this.colors[level.color]('%s') + '\n' + this.colors[level.color]('%s'),
         obj.time.toUTCString(),
         level.name,
         obj.err.message,
         obj.err.stack);
     } else {
       // Log standard message.
-      console.log('[%s] ' + colors[level.color]('[%s]') + ': ' + colors[level.color]('%s'),
+      console.log('[%s] ' + this.colors[level.color]('[%s]') + ': ' + this.colors[level.color]('%s'),
         obj.time.toUTCString(),
         level.name,
         obj.msg);
@@ -79,5 +78,4 @@ function createLevel(name, index, color) {
  * ******************** Exports
  * ************************************************** */
 
-exports = module.exports = PrettyStream;
-exports = PrettyStream;
+module.exports = PrettyStream;
